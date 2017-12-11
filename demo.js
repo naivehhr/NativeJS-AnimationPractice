70d39f9768d5fa39889d203f401fd559abe04f5e
naivehhr <1903283729@qq.com>
2017-12-11 23:49:30 +0800

    .

---
+++ demo.js
@@ -0,0 +1,34 @@
+window.myAnimation = (function(){
+  var imgUrl = './img/rabbit-big.png'
+  var positions = [
+    '0 -854',
+    '-174 -852',
+    '-329 -852',
+    '-524 -852',
+    '-698 -852',
+    '-873 -848'
+  ]
+  var ele = document.getElementById('rabbit')
+  // animation(ele, positions, imgUrl)
+  function animation() {
+    var index = 0;
+    ele.style.backgroundImage = `url(${imgUrl})`
+    ele.style.backgroundRepeat = 'np-repeat'
+    function run(){
+      var posion = positions[index].split(' ');
+      ele.style.backgroundPosition = posion[0] + 'px '+ posion[1] + 'px'
+      index++;
+      if(index >= positions.length) index = 0
+      setTimeout(run, 80)
+    }
+    run()
+  }
+  return {
+    animation: animation
+  }
+})()
+
+
+!function(){
+  window.huhaoran = 'huhaoran'
+}()
